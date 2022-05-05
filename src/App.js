63e26d9071c023b9921d8c964967cdde75ecbd45
Michal Plewa <themipsial@gmail.com>
2022-05-05 23:05:32 +0200

    styles update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Button from './components/Button/Button.js';
 import Container from './components/Container/Container.js';
 import Time from './components/Time/Time.js';
 import { useState, useEffect } from 'react';
+import styles from './components/Time/time.module.scss';
 
 function App() {
   const [time, setTime] = useState(0);
-  const [timer ,setTimer] = useState(false);
+  const [timer, setTimer] = useState(false);
 
   useEffect(() => {
     // code that runs once at the start
@@ -25,10 +26,10 @@ function App() {
   return (
     <Container>
       <Time duration={time} />
-      <div>
-        <Button onClick={() => setTimer(false)}>Start</Button>
-        <Button onClick={() => setTimer(true)}>Stop</Button>
-        <Button onClick={() => setTime(0)}>Reset</Button>
+      <div className={styles.time}>
+      <Button onClick={() => setTimer(false)}>Start</Button>
+      <Button onClick={() => setTimer(true)}>Stop</Button>
+      <Button onClick={() => setTime(0)}>Reset</Button>
       </div>
     </Container>
   );
