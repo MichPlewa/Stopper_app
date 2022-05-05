a37399bb19920d76ef0393cb3fa1cae43c9a923c
Michal Plewa <themipsial@gmail.com>
2022-05-05 22:58:21 +0200

    setInterval updata

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import Button from './components/Button/Button.js';
 import Container from './components/Container/Container.js';
 import Time from './components/Time/Time.js';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-
   const [time, setTime] = useState(0);
-  const setTimer = null;
+  const [timer ,setTimer] = useState(false);
 
   useEffect(() => {
     // code that runs once at the start
     const start = () => {
-      setTimer(setInterval(() => {
-      setTime(prevValue => prevValue + 1);
-      }, 1))
+      setTimer(
+        setInterval(() => {
+          setTime((prevValue) => prevValue + 1);
+        }, 1)
+      );
     };
     return () => {
-       // code that runs once at the end
-       if(time) clearInterval(time);
+      // code that runs once at the end
+      if (timer) clearInterval(time);
     };
   }, []);
 
   return (
     <Container>
-      <Time duration={30000}/>
+      <Time duration={time} />
       <div>
-        <Button>Start</Button>
-        <Button>Stop</Button>
-        <Button>Reset</Button>
+        <Button onClick={() => setTimer(false)}>Start</Button>
+        <Button onClick={() => setTimer(true)}>Stop</Button>
+        <Button onClick={() => setTime(0)}>Reset</Button>
       </div>
     </Container>
   );
